3fcd0e03df03bd6917b8bd789ffba803a30e8eda
Pablo Velez <Pablovelez@AL-COL-02313.local>
2018-04-03 21:07:48 -0500

    Continuing the practice, now with async functions, callback, promises and Rxjs Observables

---
+++ ui/js/multilevel.js
@@ -0,0 +1,114 @@
+/* Repasando Callbacks */
+(() => {
+    var textoH3 = document.querySelector("#async-callback div.texto");
+    var boton = textoH3.parentElement.querySelector("#action");
+
+    const setTexto = texto => {
+        textoH3.textContent = texto;
+    }
+
+    const checkAuth = callback => {
+        setTexto("Verificando identidad...");
+        setTimeout(() => {
+            callback(true);
+        }, 2000);
+    };
+
+    const fetchName = callback => {
+        setTexto("Obteniendo el usuario...");
+        setTimeout(() => {
+            callback({nombre : "Pablo Andres Velez Vidal"});
+        }, 2000);
+    }
+
+    boton.addEventListener("click", () => {
+        checkAuth(auth => {
+            if (auth) {
+                fetchName(user => {
+                    setTexto(user.nombre);
+                }) 
+            }
+        });
+    });
+})();
+
+/* Repasando Promises */
+(() => {
+    var textoH3 = document.querySelector("#async-promises div.texto");
+    var boton = textoH3.parentElement.querySelector("#action");
+
+    const setTexto = texto => {
+        textoH3.textContent = texto;
+    }
+
+    const checkAuth = () => {
+        return new Promise((resolve, reject) => {
+            setTexto("Verificando identidad...");
+            setTimeout(() => {
+                resolve(true);
+            }, 2000);
+        })
+
+    };
+
+    const fetchName = () => {
+        return new Promise((resolve, reject) => {
+            setTexto("Obteniendo el usuario...");
+            setTimeout(() => {
+                resolve({nombre : "Pablo Andres Velez Vidal"});
+            }, 2000);
+        })
+    }
+
+    boton.addEventListener("click", () => {
+        checkAuth()
+        .then( auth => {
+            if (auth) {
+                return fetchName();
+            }
+        })
+        .then( user => {
+            setTexto(user.nombre);
+        });
+    });
+})();
+
+/* Repasando Observables */
+(() => {
+    var textoH3 = document.querySelector("#async-observables div.texto");
+    var boton = textoH3.parentElement.querySelector("#action");
+
+    const setTexto = texto => {
+        textoH3.textContent = texto;
+    }
+
+    const checkAuth = () => {
+        return Rx.Observable.create((observer) => {
+            setTexto("Verificando identidad...");
+            setTimeout(() => {
+                observer.next(true);
+            }, 2000);
+        })
+
+    };
+
+    const fetchName = () => {
+        return Rx.Observable.create((observer) => {
+            setTexto("Obteniendo el usuario...");
+            setTimeout(() => {
+                observer.next({nombre : "Pablo Andres Velez Vidal"});
+            }, 2000);
+        })
+    }
+
+    Rx.Observable.fromEvent(boton, 'click')
+        .switchMap( event => checkAuth())
+        .switchMap( isAuth => {
+            if (isAuth) {
+                return fetchName();
+            }
+        })
+        .subscribe(user => {
+            setTexto(user.nombre)
+        })
+})();
