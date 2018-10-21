a2699e25afc8f4ebef929de37e415bb359e71cd6
Pablo Velez <pvelez@bp-3.com>
2018-10-20 22:20:54 -0500

    Practice vid 4

---
+++ rxjs/vid_4.js
@@ -0,0 +1,28 @@
+const request = new Request('http://localhost:3300/api/v1/sports', {
+    method: 'get',
+    headers: {
+        Accept: 'application/json'
+    }
+});
+
+const miPromise = new Promise((resolve, reject) => {
+    fetch(request)
+    .then(response => {
+        if (response.ok) {
+            response.json().then(json => {
+            console.log('Promise resolved');
+            resolve(json);
+          });
+        }
+    })
+    .catch((error) => {
+        console.log('promesa rechazada');
+        reject(error);
+    })
+});
+
+const miObs = Rx.Observable.fromPromise(miPromise);
+
+miObs.subscribe((res) => {
+    console.log('Resultados: ->', res);
+});
