67059d5972399f21785a1fd262fb141b8b103a5e
Pablo Velez <pvelez@bp-3.com>
2020-09-29 17:44:06 +0200

    modify the loop by using the for...in and the initial definition of res to use default if null

--- AlgoCast/maxchar/index.js
+++ AlgoCast/maxchar/index.js
@@ -7,22 +7,31 @@
 
 function maxChar(str) {
   let strArr = str.toString().split("");
-  let res = [];
+  //   let res = [];
+  let res = {};
+
   strArr.map((char) => {
-    if (!res[char]) {
-      res[char] = 0;
-    }
-    res[char] = res[char] + 1;
+    // if (!res[char]) {
+    //   res[char] = 0;
+    // }
+    res[char] = res[char] + 1 || 1;
   });
   console.log(res);
   var maxChar = "";
   let maxVal = 0;
-  for (const [key, value] of Object.entries(res)) {
-    if (value > maxVal) {
-      maxChar = key;
-      maxVal = value;
+  //   for (const [key, value] of Object.entries(res)) {
+  //     if (value > maxVal) {
+  //       maxChar = key;
+  //       maxVal = value;
+  //     }
+  //   }
+  for (let char in res) {
+    if (res[char] > maxVal) {
+      maxVal = res[char];
+      maxChar = char;
     }
   }
+
   return maxChar;
 }
 
