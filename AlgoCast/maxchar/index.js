7910147531f1a4bc16718873740f95a4f8702c88
Pablo Velez <pvelez@bp-3.com>
2020-09-29 17:31:25 +0200

    add the solution to the most repetitive character problem

--- AlgoCast/maxchar/index.js
+++ AlgoCast/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let strArr = str.toString().split("");
+  let res = [];
+  strArr.map((char) => {
+    if (!res[char]) {
+      res[char] = 0;
+    }
+    res[char] = res[char] + 1;
+  });
+  console.log(res);
+  var maxChar = "";
+  let maxVal = 0;
+  for (const [key, value] of Object.entries(res)) {
+    if (value > maxVal) {
+      maxChar = key;
+      maxVal = value;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
