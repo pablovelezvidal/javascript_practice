74fce917adae425b104c4221c98d45233e520eb9
Pablo Velez <pvelez@bp-3.com>
2020-10-01 19:17:00 +0200

    add the solution 1 to the chunk array problem

--- AlgoCast/chunk/index.js
+++ AlgoCast/chunk/index.js
@@ -8,6 +8,19 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let res = [];
+  let temp = [];
+  array.map((el, i) => {
+    if (temp.length < size) {
+      temp.push(el);
+      if (temp.length == size || i == array.length - 1) {
+        res.push(temp);
+        temp = [];
+      }
+    }
+  });
+  return res;
+}
 
 module.exports = chunk;
