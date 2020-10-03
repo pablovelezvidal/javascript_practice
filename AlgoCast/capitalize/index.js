e6b3608ad5bea23be9fc68ee8dfdec1458adf1bf
Pablo Velez <pvelez@bp-3.com>
2020-10-03 10:50:13 +0200

    Add a second solution, not elegant but a different approach

--- AlgoCast/capitalize/index.js
+++ AlgoCast/capitalize/index.js
@@ -8,12 +8,31 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  return str
-    .split(" ")
-    .map((word) => {
-      return word[0].toUpperCase() + word.slice(1);
-    })
-    .join(" ");
+  // if first character is not a letter, include it and capitalize second character
+  let result = str[0].match(/[a-zA-Z]/g)
+    ? str[0].toUpperCase()
+    : str[0] + str[1].toUpperCase();
+
+  const resultLength = result.length;
+
+  for (let i = resultLength; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
 }
+
+// function capitalize(str) {
+//   return str
+//     .split(" ")
+//     .map((word) => {
+//       return word[0].toUpperCase() + word.slice(1);
+//     })
+//     .join(" ");
+// }
 
 module.exports = capitalize;
