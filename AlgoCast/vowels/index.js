5d7a3242cbd1a3a0f52c80d15ca1bff57b034bc3
Pablo Velez <pvelez@bp-3.com>
2020-10-19 15:56:55 +0200

    Add the initial iterative solution to the vowel counter

--- AlgoCast/vowels/index.js
+++ AlgoCast/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let counter = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  str
+    .toLowerCase()
+    .split("")
+    .map((c) => {
+      debugger;
+      if (vowels.indexOf(c) !== -1) {
+        counter++;
+      }
+    });
+  return counter;
+}
 
 module.exports = vowels;
